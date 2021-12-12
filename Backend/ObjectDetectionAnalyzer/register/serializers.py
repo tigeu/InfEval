79e9cda2477b337f531c67d1db362f6d8cbfd62c
tigeu100 <tigeu100@hhu.de>
2021-12-12 15:35:33 +0100

    Remove custom create method from UserSerializer

--- Backend/ObjectDetectionAnalyzer/register/serializers.py
+++ Backend/ObjectDetectionAnalyzer/register/serializers.py
@@ -6,10 +6,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email']
-
-    def create(self, validated_data):
-        user = super(UserSerializer, self).create(validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
